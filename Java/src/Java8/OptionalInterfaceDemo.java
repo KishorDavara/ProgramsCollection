dc41e59e7580188f7492bb7872c2ea57d5034983
Kishor Davara <kishordavara.patel49@gmail.com>
2019-01-07 23:21:46 +0530

    chore:Optional interface demo

---
+++ Java/src/Java8/OptionalInterfaceDemo.java
@@ -0,0 +1,50 @@
+package Java8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+class Artist {
+	String name;
+
+	Artist(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+}
+
+public class OptionalInterfaceDemo {
+    public static List<Artist> artists;
+    
+    
+    public Optional<Artist> getArtist(int index) {
+      if(index < 0 || index >= artists.size()) {
+    	  return Optional.empty(); //if not  artist found returns the empty Optional instead of null
+      }
+      return Optional.of(artists.get(index));
+    }
+    
+    public String getArtistName(int index) {
+    	Optional<Artist> artist = getArtist(index);
+    	return artist.map(Artist::getName).orElse("Artist not found");   //if get the empty Optional  returns the "Artist not found"
+    }
+    
+	public static void main(String[] args) { 	
+	 OptionalInterfaceDemo demo = new OptionalInterfaceDemo();
+	 artists = new ArrayList<>();
+	 artists.add(new Artist("Kishor"));
+	 artists.add(new Artist("Harsh"));
+	 artists.add(new Artist("Harshad"));
+	 
+	 System.out.println(demo.getArtistName(-1));
+	 System.out.println(demo.getArtistName(0));
+	 System.out.println(demo.getArtistName(1));
+	 System.out.println(demo.getArtistName(2));
+	 System.out.println(demo.getArtistName(3));
+    
+	}
+
+}
